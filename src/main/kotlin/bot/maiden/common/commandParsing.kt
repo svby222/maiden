76f9e5fbe9159fa2b4ed2ed982b1068176b8d6f9
Wren <54884403+musubii@users.noreply.github.com>
2021-06-13 00:26:12 +0200

    Add Arg data class + initial parsing from string

---
+++ src/main/kotlin/bot/maiden/common/commandParsing.kt
@@ -0,0 +1,103 @@
+package bot.maiden.common
+
+data class Arg(
+    val stringValue: String,
+    val leadingSpaces: Int,
+    val quoted: Boolean,
+
+    val convertedValue: Any,
+) {
+//    override fun toString() = buildString {
+//        append(" ".repeat(leadingSpaces))
+//        if (quoted) append('"')
+//        append(stringValue)
+//        if (quoted) append('"')
+//    }
+}
+
+internal fun parseArguments(args: String): List<Arg> {
+    val results = mutableListOf<Arg>()
+
+    var current = StringBuilder()
+    var leadingSpaces = 0
+    var quoted = false
+
+    fun pushCurrent(): Boolean {
+        if (current.isEmpty()) return false
+
+        val string = current.toString()
+        results.add(
+            Arg(
+                string,
+                leadingSpaces,
+                quoted,
+
+                string
+            )
+        )
+
+        current = StringBuilder()
+        leadingSpaces = 0
+
+        return true
+    }
+
+    for (char in args) {
+        when (char) {
+            ' ' -> {
+                if (!quoted) {
+                    pushCurrent()
+                    leadingSpaces++
+                } else {
+                    current.append(char)
+                }
+            }
+            '"' -> {
+                pushCurrent()
+                quoted = !quoted
+            }
+            else -> current.append(char)
+        }
+    }
+
+    // TODO: make trimming quoted args the default behavior
+
+    // If still quoted, append the unfinished quote to the previous argument
+    // Otherwise, push as a separate arg
+    if (quoted) {
+        val string = "\"" + current.toString()
+        val lastArg = results.lastOrNull()
+        if (lastArg?.quoted == false) {
+            val mergedValue = lastArg.stringValue + string
+            results[results.lastIndex] = lastArg.copy(
+                stringValue = mergedValue,
+                convertedValue = mergedValue
+            )
+        } else {
+            results.add(
+                Arg(
+                    string,
+                    leadingSpaces,
+                    false,
+
+                    string
+                )
+            )
+        }
+    } else {
+        pushCurrent()
+    }
+
+    return results
+}
+
+internal fun reconstructArgumentString(args: List<Arg>): String {
+    return args.joinToString("") {
+        buildString {
+            append(" ".repeat(it.leadingSpaces))
+            if (it.quoted) append('"')
+            append(it.stringValue)
+            if (it.quoted) append('"')
+        }
+    }.trim()
+}
