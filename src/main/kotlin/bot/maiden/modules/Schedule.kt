b3555afdd4a86651c13dc7c183a6d5cd8452dc47
Wren <54884403+musubii@users.noreply.github.com>
2021-07-07 16:33:46 -1000

    Rewrite schedule with new dialog modal implementation
    - Add scheduled command to list and remove scheduled events

--- src/main/kotlin/bot/maiden/modules/Schedule.kt
+++ src/main/kotlin/bot/maiden/modules/Schedule.kt
@@ -2,18 +2,26 @@ package bot.maiden.modules
 
 import bot.maiden.*
 import bot.maiden.model.GuildScheduledEvent
-import bot.maiden.utilities.MultistepDialog
-import bot.maiden.utilities.multistepDialog
+import bot.maiden.modules.modal.DialogStepModal
+import bot.maiden.modules.modal.Modals
+import bot.maiden.modules.modal.StepModal
+import bot.maiden.modules.modal.buildDialog
 import kotlinx.coroutines.*
 import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.Emoji
 import net.dv8tion.jda.api.exceptions.ErrorResponseException
+import net.dv8tion.jda.api.interactions.components.ButtonStyle
 import org.slf4j.LoggerFactory
 import java.lang.reflect.InvocationTargetException
+import java.time.Duration
 import java.time.Instant
+import java.time.temporal.ChronoUnit
+import kotlin.math.max
+import kotlin.math.min
 
 object Schedule : Module {
     private val LOGGER = LoggerFactory.getLogger(Schedule::class.java)
-    private val scope = CoroutineScope(Dispatchers.Default)
+    private var scope = CoroutineScope(Dispatchers.Default)
 
     override suspend fun initialize(bot: Bot) {
         bot.database.withSession {
@@ -149,28 +157,37 @@ object Schedule : Module {
 
         val trimmed = (if (command.startsWith("m!")) command.substring(2) else command).trim()
 
-        val dialog = multistepDialog {
+        var interval = 0L
+        val dialog = buildDialog {
             title = "Scheduler"
 
-            var interval = 0L
+            addStep {
+                optionMode = DialogStepModal.DialogStep.OptionMode.Text
 
-            step {
-                title = "`$trimmed`"
+                val truncated = trimmed.substringBefore(' ').let {
+                    if (it.length > 64) it.take(61) + "..."
+                    else it
+                }
 
-                text = """
-                    How often should that command be executed?
-                """.trimIndent()
+                title = "Scheduling `$truncated`"
 
+                mainText = "How often should that command be executed?"
                 optionsText = """
                     Enter the interval in the format `x days x hours x minutes`.
                     **Note: the minimum interval is 3 hours.**
                 """.trimIndent()
 
-                cancelOption("cancel")
+                option(
+                    DialogStepModal.StepOption(
+                        "Interval (enter text)",
+                        data = DialogStepModal.StepOption.FreeInputData
+                    )
+                )
+                option(DialogStepModal.StepOption("cancel", data = DialogStepModal.StepOption.CancelData))
 
-                onResponse { _, data ->
-                    val matches = INTERVAL_REGEX.matchEntire(data as String)
-                        ?: return@onResponse MultistepDialog.StepResult.Invalid
+                onComplete { _, data, _ ->
+                    val matches = INTERVAL_REGEX.matchEntire(data)
+                        ?: return@onComplete StepModal.StepResult.Invalid
 
                     val days = matches.groups[1]?.value?.toIntOrNull() ?: 0
                     val hours = matches.groups[2]?.value?.toIntOrNull() ?: 0
@@ -181,15 +198,15 @@ object Schedule : Module {
 
                     if (!context.requester.isOwner(context.bot) && interval < 3 * 3600L) {
                         interval = 0
-                        return@onResponse MultistepDialog.StepResult.Invalid
+                        return@onComplete StepModal.StepResult.Invalid
                     }
 
-                    MultistepDialog.StepResult.Next
+                    StepModal.StepResult.GotoNext
                 }
             }
 
-            onFinish {
-                context.replyAsync("Ok, scheduling every $interval s")
+            addStep(dynamic = true) {
+                mainText = "Ok, scheduling every ${Duration.ofSeconds(interval).toPrettyString()}."
 
                 val event = GuildScheduledEvent().apply {
                     this.guildId = context.guild.idLong
@@ -211,9 +228,8 @@ object Schedule : Module {
             }
         }
 
-        if (!Dialog.beginDialog(context.channel, context.requester, dialog)) {
-            context.replyAsync("There is already an active assistant")
-        }
+        Modals.beginModal(context.channel, context, dialog)
+            .join()
     }
 
     @Command
@@ -243,7 +259,168 @@ object Schedule : Module {
             }
         }
 
+        // TODO store events and stop them individually rather than reinitializing the entire module
+        scope.cancel()
+        scope = CoroutineScope(Dispatchers.Default)
+        initialize(context.bot)
+
         context.replyAsync("$count scheduled events were deleted.")
+    }
+
+    @Command
+    @HelpText(
+        "Displays a list of all scheduled events, with the option to delete individual events. Only available to server administrators.",
+        group = "schedule"
+    )
+    suspend fun scheduled(context: CommandContext) {
+        context.requester ?: return
+
+        if (!context.requester.isOwner(context.bot) &&
+            context.guild.getMember(context.requester)?.permissions?.contains(Permission.ADMINISTRATOR) != true
+        ) {
+            // TODO actual permission handling
+            context.replyAsync("You can't do that (not an administrator)")
+            return
+        }
+
+        // TODO db pagination? Stop fetching all at once
+        val events = context.bot.database.withSession {
+            it.createQuery("from GuildScheduledEvent where guild_id = :guild_id")
+                .setParameter("guild_id", context.guild.idLong)
+                .resultList
+        }.filterIsInstance<GuildScheduledEvent>()
+
+        val chunks = events.chunked(5)
+        var currentChunkIndex = 0
+
+        val dialog = buildDialog {
+            title = "Scheduled events (${events.size})"
+
+            if (events.isEmpty()) {
+                addStep {
+                    mainText = "There are no events scheduled in this server."
+                    defaultResult = StepModal.StepResult.Finish
+                }
+            }
+
+            addStep(dynamic = true) {
+                replacePrevious = true
+
+                val previous =
+                    option(
+                        DialogStepModal.StepOption("Previous", icon = Emoji.fromMarkdown("⬅️"))
+                    )
+                val next =
+                    option(
+                        DialogStepModal.StepOption("Next", icon = Emoji.fromMarkdown("➡️"))
+                    )
+                val delete =
+                    option(
+                        DialogStepModal.StepOption(
+                            "Remove one",
+                            icon = Emoji.fromMarkdown("🚮"),
+                            buttonStyle = ButtonStyle.DANGER
+                        )
+                    )
+
+                // TODO auto-close
+                val close =
+                    option(
+                        DialogStepModal.StepOption(
+                            "Done",
+                            icon = Emoji.fromMarkdown("✅"),
+                            buttonStyle = ButtonStyle.SUCCESS
+                        )
+                    )
+
+                editEmbed {
+                    for ((index, event) in chunks[currentChunkIndex].withIndex()) {
+                        addField("#${index + 1}: `${event.command}`", buildString {
+                            val durationPretty = Duration.ofSeconds(event.intervalSeconds).toPrettyString()
+
+                            appendLine("Requested by <@${event.requesterId}>")
+                            appendLine("Scheduled on ${event.startAt}")
+                            appendLine("Executes every $durationPretty in <#${event.channelId}>")
+
+                            val now = Instant.now()
+                            var nextExecutionTime = event.startAt!!
+                            while (nextExecutionTime.isBefore(now)) {
+                                nextExecutionTime = nextExecutionTime.plus(event.intervalSeconds, ChronoUnit.SECONDS)
+                            }
+
+                            appendLine("Next execution on $nextExecutionTime")
+                        }, false)
+                    }
+                }
+
+                onComplete { option, _, _ ->
+                    when (option) {
+                        previous -> {
+                            currentChunkIndex = max(0, currentChunkIndex - 1)
+                        }
+                        next -> {
+                            currentChunkIndex = min(chunks.lastIndex, currentChunkIndex + 1)
+                        }
+
+                        delete -> {
+                            return@onComplete StepModal.StepResult.GotoNext
+                        }
+                        close -> {
+                            return@onComplete StepModal.StepResult.Finish
+                        }
+                    }
+
+                    StepModal.StepResult.GotoCurrent
+                }
+            }
+
+            var targetEvent = 0
+            addStep(dynamic = true) {
+                optionMode = DialogStepModal.DialogStep.OptionMode.Text
+
+                mainText = "Which event should be removed?"
+                optionsText = "Respond with a number from 1 to ${chunks[currentChunkIndex].size}."
+
+                optionMode = DialogStepModal.DialogStep.OptionMode.Text
+
+                option(
+                    DialogStepModal.StepOption(
+                        "Index (enter text)",
+                        data = DialogStepModal.StepOption.FreeInputData
+                    )
+                )
+                option(DialogStepModal.StepOption("cancel", data = DialogStepModal.StepOption.CancelData))
+
+                onComplete { _, response, _ ->
+                    val intValue = response.toIntOrNull().takeIf { (it) in 1..chunks[currentChunkIndex].size }
+                    if (intValue == null) StepModal.StepResult.Invalid
+                    else {
+                        targetEvent = intValue - 1
+                        val event = chunks[currentChunkIndex][targetEvent]
+
+                        context.bot.database.withSession {
+                            it.beginTransaction().let { tx ->
+                                it.delete(event)
+                                tx.commit()
+                            }
+                        }
+
+                        // TODO store events and stop them individually rather than reinitializing the entire module
+                        scope.cancel()
+                        scope = CoroutineScope(Dispatchers.Default)
+                        initialize(context.bot)
+
+                        StepModal.StepResult.GotoNext
+                    }
+                }
+            }
+
+            addStep {
+                mainText = "The selected event has been deleted."
+            }
+        }
+
+        Modals.beginModal(context.channel, context, dialog).join()
     }
 
     override fun close() {
